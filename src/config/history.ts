d19e3c3957e8c63e0dfc1d17c1e52f71ea6b94da
JaniceZD <mnzd123@gmail.com>
2020-10-06 17:44:08 +0800

    修复页面显示bug

--- src/config/history.ts
+++ src/config/history.ts
@@ -1,5 +1,19 @@
-import { createBrowserHistory } from 'history'
+import {createBrowserHistory} from 'history'
 
-const history = createBrowserHistory()
+// const history = createBrowserHistory()
+
+const ENV = process.env.NODE_ENV;
+
+let publicUrl: string = "";
+
+if (ENV === "development") {
+    publicUrl = "/"
+} else if (ENV === "production") {
+    publicUrl = "/potato-demo-test"
+}
+
+const history = createBrowserHistory({
+    basename: publicUrl
+});
 
 export default history;
