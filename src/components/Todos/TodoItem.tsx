bc909178a88a1cc25cd70ce617a30ad5e07284f9
JaniceZD <mnzd123@gmail.com>
2020-10-06 22:08:00 +0800

    添加最近完成Todo收纳功能

--- src/components/Todos/TodoItem.tsx
+++ src/components/Todos/TodoItem.tsx
@@ -1,92 +1,92 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { updateTodo, editTodo } from '../../redux/actions/todos'
+import {connect} from 'react-redux'
+import {updateTodo, editTodo} from '../../redux/actions/todos'
 import classNames from 'classnames'
 import axios from '../../config/axios'
-import { Checkbox, Icon } from 'antd';
+import {Checkbox, Icon} from 'antd';
 import './TodoItem.scss'
 
 interface ITodoItemProps {
-  id: number;
-  description: string;
-  completed: boolean;
-  editing: boolean;
-  updateTodo: (payload: any) => any;
-  editTodo: (id: number) => any;
+    id: number;
+    description: string;
+    completed: boolean;
+    editing: boolean;
+    updateTodo: (payload: any) => any;
+    editTodo: (id: number) => any;
 }
 
 interface ITodoItemState {
-  editText: string;
+    editText: string;
 }
 
 class TodoItem extends React.Component<ITodoItemProps, ITodoItemState> {
-  constructor(props: ITodoItemProps) {
-    super(props)
-    this.state = {
-      editText: this.props.description
+    constructor(props: ITodoItemProps) {
+        super(props)
+        this.state = {
+            editText: this.props.description
+        }
     }
-  }
 
-  updateTodo = async (params: any) => {
-    try {
-      const response = await axios.put(`todos/${this.props.id}`, params)
-      this.props.updateTodo(response.data.resource)
-    } catch (e) {
-      throw new Error(e)
+    updateTodo = async (params: any) => {
+        try {
+            const response = await axios.put(`todos/${this.props.id}`, params)
+            this.props.updateTodo(response.data.resource)
+        } catch (e) {
+            throw new Error(e)
+        }
     }
-  }
 
-  editTodo = () => {
-    this.props.editTodo(this.props.id)
-  }
-
-  onKeyUp = (e: any) => {
-    if (e.keyCode === 13 && this.state.editText !== '') {
-      this.updateTodo({ description: this.state.editText })
+    editTodo = () => {
+        this.props.editTodo(this.props.id)
     }
-  }
 
-  render() {
-    const Editing = (
-      <div className="editing">
-        <input type="text"
-          value={this.state.editText}
-          onChange={e => this.setState({ editText: e.target.value })}
-          onKeyUp={this.onKeyUp}
-        />
-        <div className="iconWrapper">
-          <Icon type="enter" onClick={e => this.updateTodo({ description: this.state.editText })} />
-          <Icon type="delete" onClick={e => this.updateTodo({ deleted: true })} />
-        </div>
-      </div>
-    )
+    onKeyUp = (e: any) => {
+        if (e.keyCode === 13 && this.state.editText !== '') {
+            this.updateTodo({description: this.state.editText})
+        }
+    }
 
-    const Text = <span className="text" onDoubleClick={this.editTodo}>{this.props.description}</span>
+    render() {
+        const Editing = (
+            <div className="editing">
+                <input type="text"
+                       value={this.state.editText}
+                       onChange={e => this.setState({editText: e.target.value})}
+                       onKeyUp={this.onKeyUp}
+                />
+                <div className="iconWrapper">
+                    <Icon type="enter" onClick={e => this.updateTodo({description: this.state.editText})}/>
+                    <Icon type="delete" onClick={e => this.updateTodo({deleted: true})}/>
+                </div>
+            </div>
+        )
 
-    const todoItemClass = classNames({
-      TodoItem: true,
-      editing: this.props.editing,
-      completed: this.props.completed
-    })
-    return (
-      <div className={todoItemClass} id="TodoItem">
-        <Checkbox
-          className="checkbox"
-          checked={this.props.completed}
-          onChange={e => this.updateTodo({ completed: e.target.checked })} />
-        {this.props.editing ? Editing : Text}
-      </div>
-    )
-  }
+        const Text = <span className="text" onDoubleClick={this.editTodo}>{this.props.description}</span>
+
+        const todoItemClass = classNames({
+            TodoItem: true,
+            editing: this.props.editing,
+            completed: this.props.completed
+        })
+        return (
+            <div className={todoItemClass} id="TodoItem">
+                <Checkbox
+                    className="checkbox"
+                    checked={this.props.completed}
+                    onChange={e => this.updateTodo({completed: e.target.checked})}/>
+                {this.props.editing ? Editing : Text}
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state: any, ownProps: any) => ({
-  ...ownProps
+    ...ownProps
 })
 
 const mapDispatchToProps = {
-  updateTodo,
-  editTodo
+    updateTodo,
+    editTodo
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoItem)
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,56 +1,95 @@
+import {Icon, Tree} from "antd"
 import React from 'react';
-import { connect } from 'react-redux'
+import axios from '../../config/axios'
+import {connect} from 'react-redux'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
-import { updateTodo } from '../../redux/actions/todos'
+import {editTodo, updateTodo} from '../../redux/actions/todos'
 import './Todos.scss'
 
+const {TreeNode} = Tree;
+
 class Todos extends React.Component<any> {
-  constructor(props: any) {
-    super(props)
-    console.log(props)  //props 是 {todos: [], function initTodos(), function updateTodo()}
-  }
+    constructor(props: any) {
+        super(props)
+        console.log(props)  //props 是 {todos: [], function initTodos(), function updateTodo()}
+    }
 
-  get unDeletedTodos() {
-    return this.props.todos.filter((t: any) => !t.deleted)
-  }
+    get unDeletedTodos() {
+        return this.props.todos.filter((t: any) => !t.deleted)
+    }
 
-  get unCompletedTodos() {
-    return this.unDeletedTodos.filter((t: any) => !t.completed)
-  }
+    get unCompletedTodos() {
+        return this.unDeletedTodos.filter((t: any) => !t.completed)
+    }
 
-  get completedTodos() {
-    return this.unDeletedTodos.filter((t: any) => t.completed)
-  }
+    get completedTodos() {
+        return this.unDeletedTodos.filter((t: any) => t.completed)
+    }
 
-  render() {
-    return (
-      <div className="Todos" >
-        <TodoInput />
-        <div className="todoLists">
-          {
-            this.unCompletedTodos.map((t: any) =>
-              <TodoItem key={t.id} {...t} />
-            )
-          }
-          {
-            this.completedTodos.map((t: any) =>
-              <TodoItem key={t.id} {...t} />
-            )
-          }
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="Todos">
+                <TodoInput/>
+                <div className="todoLists">
+                    {
+                        this.unCompletedTodos.map((t: any) =>
+                            <TodoItem key={t.id} {...t} update={this.postTodo}
+                                      editTodo={this.isTodoEditable}/>
+                        )
+                    }
+                    <Tree
+                        showLine
+                        switcherIcon={<Icon type="down"/>}
+                    >
+                        <TreeNode title="最近完成的任务" key="0-0-0-2">
+                            {
+                                this.completedTodos.map((t: any) => {
+                                        return (
+                                            <TreeNode key={`tree-${t.id}`} title={
+                                                <TodoItem
+                                                    key={t.id}
+                                                    {...t}
+                                                    update={this.postTodo}
+                                                    editTodo={this.isTodoEditable}
+                                                />
+                                            }/>
+                                        )
+                                    }
+                                )
+                            }
+                        </TreeNode>
+                    </Tree>
+                </div>
+            </div>
+        )
+    }
+
+    isTodoEditable = (id: number) => {
+        this.props.editTodo(id)
+    }
+
+    postTodo = async (id: number, params: any) => {
+        if (params.completed) {
+            params.completed_at = new Date()
+        }
+        try {
+            const response = await axios.put(`todos/${id}`, params)
+            this.props.updateTodo(response.data.resource)
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
 }
 
 const mapStateToProps = (state: any, ownProps: any) => ({
-  todos: state.todos,
-  ...ownProps
+    todos: state.todos,
+    ...ownProps
 })
 
 const mapDispatchToProps = {
-  updateTodo
+    editTodo,
+    updateTodo
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Todos);
