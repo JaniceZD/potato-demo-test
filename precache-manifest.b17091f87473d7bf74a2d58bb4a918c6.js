58bf1640df97a8ada740f99ac2d24ca3679a46ac
JaniceZD <mnzd123@gmail.com>
2020-11-23 15:10:57 +0800

    deploy

---
+++ precache-manifest.b17091f87473d7bf74a2d58bb4a918c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b843cdea434cd164f5126a65ff119e6b",
+    "url": "/potato-demo-test/index.html"
+  },
+  {
+    "revision": "cb1ab464cd425909e43b",
+    "url": "/potato-demo-test/static/css/2.5b11aa9c.chunk.css"
+  },
+  {
+    "revision": "b6772088444ab70f37f3",
+    "url": "/potato-demo-test/static/css/main.3603e77c.chunk.css"
+  },
+  {
+    "revision": "cb1ab464cd425909e43b",
+    "url": "/potato-demo-test/static/js/2.a1f28fc4.chunk.js"
+  },
+  {
+    "revision": "b6772088444ab70f37f3",
+    "url": "/potato-demo-test/static/js/main.da4fd8a2.chunk.js"
+  },
+  {
+    "revision": "bea366b139ffea08b9d1",
+    "url": "/potato-demo-test/static/js/runtime-main.11b46b27.js"
+  }
+]);
